7d56998a176da6aada9003c3a7cae4c2d603111a
Ekaterina Balueva <katerissa@mail.ru>
2022-12-08 20:41:30 +0300

    commit

--- sprint3_app/ViewController.swift
+++ sprint3_app/ViewController.swift
@@ -23,7 +23,6 @@ class ViewController: UIViewController {
     private func getCounterLabelText() -> String {
                 return counterLabelBaseText + String(counter)
             }
-    
     @IBAction func counterButtonClicked() {
         counter += 1
         counterLabel.text = getCounterLabelText()
